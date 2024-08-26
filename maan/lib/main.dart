3fdf890c9651fc45509e89ca2134d758427f1b88
Abdimaan <hamaan3858@gmail.com>
2024-08-26 10:55:53 +0300

    first time doing github

---
+++ maan/lib/main.dart
@@ -0,0 +1,65 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text("Dicee"),
+          backgroundColor: Colors.red,
+        ),
+        body: const dice(),
+      ),
+    ),
+  );
+}
+
+// ignore: camel_case_types
+class dice extends StatefulWidget {
+  const dice({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _diceState createState() => _diceState();
+}
+
+// ignore: camel_case_types
+class _diceState extends State<dice> {
+  int leftDice = 1;
+  int rightDice = 1;
+  void change() {
+    setState(() {
+      leftDice = Random().nextInt(6) + 1;
+      rightDice = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                change();
+              },
+              child: Image.asset("images/dice$leftDice.png"),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                change();
+              },
+              child: Image.asset("images/dice$rightDice.png"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
